fdaea59f51e4f87b26203182422a2605daa0e6ad
Francisco Soto <panchon.ues@gmail.com>
2024-07-29 13:51:18 -0700

    sendMessageBox; Input,Output, EventEmitter

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { ChatMessageComponent, MyMessageComponent } from '@components/index';
+import { ChatMessageComponent, MyMessageComponent, TextMessageBoxComponent, TypingLoaderComponent } from '@components/index';
 
 
 
@@ -12,9 +12,38 @@ import { ChatMessageComponent, MyMessageComponent } from '@components/index';
   imports: [
     CommonModule,
     ChatMessageComponent,
-    MyMessageComponent
+    MyMessageComponent,
+    TextMessageBoxComponent,
+    TypingLoaderComponent,
   ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: `
+    .chat-messages::-webkit-scrollbar-track
+    {
+      -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+      border-radius: 10px;
+      background-color: #F5F5F5;
+    }
+
+    .chat-messages::-webkit-scrollbar
+    {
+      width: 12px;
+      background-color: #F5F5F5;
+    }
+
+    .chat-messages::-webkit-scrollbar-thumb
+    {
+      border-radius: 10px;
+      -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+      background-color: #555;
+    }
+  `,
 })
-export default class OrthographyPageComponent { }
+export default class OrthographyPageComponent {
+
+  handleMessage(prompt: string) {
+    console.log({prompt});
+  }
+
+}
